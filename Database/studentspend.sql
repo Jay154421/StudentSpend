393967d5fe78b67fc54e1cbb670cdd6ecab5f997
Jay <jaybodiongan3@gmail.com>
2025-05-22 11:32:59 +0800

    final output

--- Database/studentspend.sql
+++ Database/studentspend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2025 at 02:35 AM
+-- Generation Time: May 22, 2025 at 05:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -39,7 +39,16 @@ CREATE TABLE `budget` (
 --
 
 INSERT INTO `budget` (`budget_id`, `budget_category`, `amount`, `user_id`) VALUES
-(17, 'Personal', 500, 6);
+(59, 'personal', 300, 11),
+(60, 'Dwqe', 120, 11),
+(68, 'eqweqwe', 1230, 11),
+(72, 'eqwe', 3000, 6),
+(74, '312312', 123, 6),
+(75, '312312e1e', 123, 6),
+(76, '123123', 2, 6),
+(77, '123123wqeqwe', 231, 6),
+(78, 'eqweq', 3, 6),
+(79, 'ewqewq', 4, 6);
 
 -- --------------------------------------------------------
 
@@ -61,7 +70,10 @@ CREATE TABLE `expense` (
 --
 
 INSERT INTO `expense` (`expense_id`, `name`, `amount`, `category`, `date`, `user_id`) VALUES
-(16, 'Jay', 150, 'Personal', '2025-03-15', 6);
+(72, 'eqwewqe', 1233123, 'personal', '2025-05-07', 11),
+(78, 'eqwek', 2000, 'eqwe', '2025-05-21', 6),
+(79, 'e12312', 3, '312312', '2025-05-22', 6),
+(80, '3123', 2, 'eqweq', '2025-05-22', 6);
 
 -- --------------------------------------------------------
 
@@ -74,6 +86,14 @@ CREATE TABLE `file` (
   `photo` varchar(150) NOT NULL,
   `user_id` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `file`
+--
+
+INSERT INTO `file` (`id`, `photo`, `user_id`) VALUES
+(20, 'images.jpg', 6),
+(21, 'balloon-7102509__340.jpg', 11);
 
 -- --------------------------------------------------------
 
@@ -92,7 +112,9 @@ CREATE TABLE `total_allowance` (
 --
 
 INSERT INTO `total_allowance` (`total_id`, `user_id`, `amount`) VALUES
-(39, 6, '5000');
+(42, 6, '4000'),
+(43, 11, '3400'),
+(44, 11, '3400');
 
 -- --------------------------------------------------------
 
@@ -115,7 +137,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `username`, `fullname`, `email`, `password`) VALUES
 (6, 'Jay154421', 'Jay Bodiongan', 'jaybodiongan0@gmail.com', '$2y$10$mcNxuQUAPNvO/SL/9wvpsuE94gB5DhHTKcKzvqWEli1bWwWXc0/2.'),
 (7, '123456', '123123', '123456@gmail.com', '$2y$10$SzH4U9LHF/jOCZyiJqnIDel47kP9SCEo7IPdq8pck/oY0g6za4jZS'),
-(9, 'jay963', 'Jay Bodiongan', 'jaybodiongan12@gmail.com', '$2y$10$O8/jnGDQqTj.JsP7cQUU2OfbKrIzxa5NsI3Jbaclv81Wv2zYgxWeO');
+(9, 'jay963', 'Jay Bodiongan', 'jaybodiongan12@gmail.com', '$2y$10$O8/jnGDQqTj.JsP7cQUU2OfbKrIzxa5NsI3Jbaclv81Wv2zYgxWeO'),
+(10, 'john154421', 'John Y. Klaro', 'john.klaro@gmail.com', '$2y$10$pjrD4NmPhabsps2i0tSsR.EFlA/oJO/nLtln4lliIYplY8fH1DdUa'),
+(11, 'onii123', 'eqwewqe', 'onii@gmail.com', '$2y$10$1Qw9O9tTAtkBUWCZKWot0Oqy9MCMccU6vwTRa/d/TMrqrNsow1JQa');
 
 --
 -- Indexes for dumped tables
@@ -161,31 +185,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `budget`
 --
 ALTER TABLE `budget`
-  MODIFY `budget_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `budget_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT for table `expense`
 --
 ALTER TABLE `expense`
-  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `expense_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- AUTO_INCREMENT for table `file`
 --
 ALTER TABLE `file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `total_allowance`
 --
 ALTER TABLE `total_allowance`
-  MODIFY `total_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `total_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
